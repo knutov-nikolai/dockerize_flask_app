d745689616d9af703be6422920b0ff2974a6ff20
nikolai knutov <nikolai.knutov@itl.team>
2024-02-10 13:48:37 +0300

    edit flask app

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ def hello():
   return '<h1>Moe Flask приложение в контейнере Docker.</h1>'
  
 if __name__ == '__main__':
-  app.run(debug=True,host='0.0.0.0', port=8000)
+  app.run(debug=True, host='0.0.0.0', port=8000)
 
